06db42cbfe1b5398bc7bb3881b5abade5750d42d
mini32stray <183096050+mini32stray@users.noreply.github.com>
2025-09-20 21:42:33 +0900

    bug fix
    - fix to play effects when bomb cuts and bad cuts
    - add effects to obstacle enters
    - move AUDIO_URLS setting area to data.js
    - pan value to have a small random deviation
    - enlarge the outside visual effect width

--- obs/data.js
+++ obs/data.js
@@ -1,3 +1,11 @@
+// This is an exaple of subdirectory style.
+const AUDIO_URLS = [
+  "sounds/miss1.mp3",
+  "sounds/miss2.mp3",
+];
+
+// This is an exaple of base64 embedding style.
 //const AUDIO_URLS = [
-//    "data:audio/mp3;base64,......",
+//  "data:audio/mp3;base64,......",
+//  "data:audio/mp3;base64,......",
 //];
